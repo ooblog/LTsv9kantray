56dafd32c14cdb7b1f247839b110be5cc574ae49
ooblog <ooblog@gmail.com>
2016-05-08 06:07:12 +0900

    「wine_sh」フォルダ内のwineパスを「Tahrpup605」に変更。都道府県アイコンとりあえず大分まで。

--- wine_sh/LTsv_printf.sh
+++ wine_sh/LTsv_printf.sh
@@ -2,4 +2,4 @@
 script_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 cd $script_dir
 cd ../LTsv
-/mnt/home/Puppy-Linux-571/wine-portable-1.7.18-1-p4/wine-portable "/mnt/home/Puppy-Linux-571/wine-portable-1.7.18-1-p4/wine-data/drive_c/Python34/python.exe LTsv_printf.py"
+/mnt/home/Tahrpup605/wine-portable-1.7.18-1-p4/wine-portable "/mnt/home/Tahrpup605/wine-portable-1.7.18-1-p4/wine-data/drive_c/Python34/python.exe LTsv_printf.py"
